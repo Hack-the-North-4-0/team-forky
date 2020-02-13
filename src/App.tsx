51c812b08eaddac4b68bedb9816a7247843eddab
sciandy <p6ukuYAs>
2020-02-13 17:32:46 +0000

    5 questionnaire pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Home} from "./Home/Home";
 import "./App.scss"
-import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Header from "./components/Header"
+import {Region} from "./Questions/Region"
+import {EmploymentStatus} from "./Questions/EmploymentStatus"
+import {Household} from "./Questions/Household"
+import {Incoming} from "./Questions/Incoming"
+import {Outgoing} from "./Questions/Outgoing"
 
 const App = () => {
   return (
     <div>
-        <Header></Header>
+      <Header/>
       <div className="App">
         <BrowserRouter>
           <Switch>
             <Route exact={true} path="/" component={Home}/>
-            <Route path="/Questionnaire/:questionId" component={Home}/>
+              <Route path="/Region" component={Region}/>
+              <Route path="/EmploymentStatus" component={EmploymentStatus}/>
+              <Route path="/Household" component={Household}/>
+              <Route path="/Incoming" component={Incoming}/>
+              <Route path="/Outgoing" component={Outgoing}/>
           </Switch>
         </BrowserRouter>
       </div>
-        <Footer/>
+      <Footer/>
     </div>
   )
 };
