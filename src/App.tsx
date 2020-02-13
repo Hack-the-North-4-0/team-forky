5407ac4d730905640211a16612be55bf77c491fc
mrmadhat <daniel@relativemarketing.co.uk>
2020-02-13 17:03:18 +0000

    Add gds design system and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.scss"
+import Header from "./Header"
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
