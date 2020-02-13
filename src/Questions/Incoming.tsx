b357437c19d4afb7be9338541bd4f507db375ab4
sciandy <p6ukuYAs>
2020-02-13 17:54:31 +0000

    Change functional components to default export.

--- src/Questions/Incoming.tsx
+++ src/Questions/Incoming.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export const Incoming = () => {
+const Incoming = () => {
     return (
         <div/>
     )
 };
+
+export default Incoming;
--- src/Questions/Outgoing.tsx
+++ src/Questions/Outgoing.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export const Outgoing = () => {
+const Outgoing = () => {
     return (
         <div/>
     )
 };
+
+export default Outgoing;
