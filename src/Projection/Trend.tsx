96436da84c87328e5bea6ced6b7fbfefd5610eff
ajum <chemistric-05@mail.ru>
2020-02-14 11:14:54 +0000

    Make trend graph dynamic

--- src/Projection/Trend.tsx
+++ src/Projection/Trend.tsx
@@ -1,30 +1,24 @@
 import React from "react"
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
+import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 
-const data = [{name: 'Feb', available: 4000},
-    {name: 'Mar', available: 3200},
-    {name: 'Apr', available: 2400},
-    {name: 'May', available: 1600},
-    {name: 'Jun', available: 800},
-    {name: 'Jul', available: 0}];
-
-const data2 = [{name: 'Feb', available: 4000.50},
-    {name: 'Mar', available: 3200.32},
-    {name: 'Apr', available: 2400},
-    {name: 'May', available: 2000},
-    {name: 'Jun', available: 1600},
-    {name: 'Jul', available: 1200}];
+const data = [{name: 'Feb', available: 8000, withPartTimeJob: 8000},
+    {name: 'Mar', available: 6400, withPartTimeJob: 6800},
+    {name: 'Apr', available: 4800, withPartTimeJob: 5600},
+    {name: 'May', available: 5000, withPartTimeJob: 6800},
+    {name: 'Jun', available: 3200, withPartTimeJob: 5100},
+    {name: 'Jul', available: 1200, withPartTimeJob: 3200}];
 
 const Trend = () => {
     return (
         <div>
-
-            <LineChart width={600} height={300} data={data2} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <Line type="monotone" dataKey="available" stroke="#8884d8" />
+                { false && <Line type="monotone" dataKey="withPartTimeJob" stroke="#82ca9d"/> }
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
+                <Legend></Legend>
             </LineChart>
             <p>Projection of your financial state in six months</p>
         </div>
