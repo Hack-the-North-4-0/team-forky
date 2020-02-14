f86c5239012c15000a99c8d7afe4c9d760720a20
mrmadhat <daniel@relativemarketing.co.uk>
2020-02-14 11:51:39 +0000

    Fixing tabs

--- src/Analysis/Analysis.tsx
+++ src/Analysis/Analysis.tsx
@@ -1,44 +1,49 @@
 import React, { FC } from "react"
-import Trend from "../Projection/Trend";
-import ActionList from "../Components/ActionList";
-import Expenses from "../Projection/Expenses";
+import Trend from "../Projection/Trend"
+import ActionList from "../Components/ActionList"
+import Expenses from "../Projection/Expenses"
 
 const Analysis: FC = () => {
-    return (
-        <div className="govuk-tabs" data-module="govuk-tabs">
-            <h2 className="govuk-tabs__title">
-                Contents
-            </h2>
-            <ul className="govuk-tabs__list">
-                <li className="govuk-tabs__list-item govuk-tabs__list-item--selected">
-                    <a className="govuk-tabs__tab" href="#expenses">
-                        Expenses
-                    </a>
-                </li>
-                <li className="govuk-tabs__list-item govuk-tabs__list-item--selected">
-                    <a className="govuk-tabs__tab" href="#projection">
-                        Projection
-                    </a>
-                </li>
-                <li className="govuk-tabs__list-item">
-                    <a className="govuk-tabs__tab" href="#actionList">
-                        Action List
-                    </a>
-                </li>
-            </ul>
-            <section className="govuk-tabs__panel" id="expenses">
-                <h2 className="govuk-heading-l">Expenses</h2>
-                <Expenses/>
-            </section>
-            <section className="govuk-tabs__panel" id="projection">
-                <h2 className="govuk-heading-l">Projection</h2>
-                <Trend/>
-            </section>
-            <section className="govuk-tabs__panel govuk-tabs__panel--hidden" id="actionList">
-                <h2 className="govuk-heading-l">Action List</h2>
-                <ActionList isTodoComplete={false} toggleTodo={() => {}}/>
-            </section>
-        </div>    )
+  return (
+    <div className="govuk-tabs" data-module="govuk-tabs">
+      <h2 className="govuk-tabs__title">Contents</h2>
+      <ul className="govuk-tabs__list">
+        <li className="govuk-tabs__list-item govuk-tabs__list-item--selected">
+          <a className="govuk-tabs__tab" href="#expenses">
+            Expenses
+          </a>
+        </li>
+        <li className="govuk-tabs__list-item govuk-tabs__list-item">
+          <a className="govuk-tabs__tab" href="#projection">
+            Projection
+          </a>
+        </li>
+        <li className="govuk-tabs__list-item">
+          <a className="govuk-tabs__tab" href="#actionList">
+            Action List
+          </a>
+        </li>
+      </ul>
+      <section className="govuk-tabs__panel" id="expenses">
+        <h2 className="govuk-heading-l">Expenses</h2>
+        <Expenses />
+      </section>
+      <section
+        className="govuk-tabs__panel govuk-tabs__panel--hidden"
+        id="projection"
+      >
+        <h2 className="govuk-heading-l">Projection</h2>
+        <Trend />
+      </section>
+      <section
+        className="govuk-tabs__panel govuk-tabs__panel--hidden"
+        id="actionList"
+      >
+        <h2 className="govuk-heading-l">Action List</h2>
+        <ActionList isTodoComplete={false} toggleTodo={() => {}} />
+      </section>
+    </div>
+  )
 }
 
-export default Analysis;
+export default Analysis
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Home from "./Home/Home";
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from "./Home/Home"
 import "./App.scss"
 import Footer from "./Components/Footer"
 import Header from "./Components/Header"
@@ -11,38 +11,41 @@ import BankDetails from "./Questions/BankDetails"
 import Result from "./Result/Result"
 import Trend from "./Projection/Trend"
 import Expenses from "./Projection/Expenses"
-import Analysis from "./Analysis/Analysis";
+import Analysis from "./Analysis/Analysis"
 
 const App = () => {
-    return (
-        <div>
-            <Header/>
-            <div className="App">
-                <div className="govuk-width-container">
-                    <main className="govuk-main-wrapper " id="main-content" role="main">
-                        <div className="govuk-grid-row">
-                            <div className="govuk-grid-column-two-thirds">
-                                <BrowserRouter>
-                                    <Switch>
-                                        <Route exact={true} path="/" component={Home}/>
-                                        <Route path="/Region" component={Region}/>
-                                        <Route path="/EmploymentStatus" component={EmploymentStatus}/>
-                                        <Route path="/Household" component={Household}/>
-                                        <Route path="/BankDetails" component={BankDetails}/>
-                                        <Route path="/Result" component={Result} />
-                                        <Route path="/Trend" component={Trend}/>
-                                        <Route path="/Expenses" component={Expenses}/>
-                                        <Route path="/Analysis" component={Analysis}/>
-                                    </Switch>
-                                </BrowserRouter>
-                            </div>
-                        </div>
-                    </main>
-                </div>
+  return (
+    <div>
+      <Header />
+      <div className="App js-enabled">
+        <div className="govuk-width-container">
+          <main className="govuk-main-wrapper " id="main-content" role="main">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-two-thirds">
+                <BrowserRouter>
+                  <Switch>
+                    <Route exact={true} path="/" component={Home} />
+                    <Route path="/Region" component={Region} />
+                    <Route
+                      path="/EmploymentStatus"
+                      component={EmploymentStatus}
+                    />
+                    <Route path="/Household" component={Household} />
+                    <Route path="/BankDetails" component={BankDetails} />
+                    <Route path="/Result" component={Result} />
+                    <Route path="/Trend" component={Trend} />
+                    <Route path="/Expenses" component={Expenses} />
+                    <Route path="/Analysis" component={Analysis} />
+                  </Switch>
+                </BrowserRouter>
+              </div>
             </div>
-            <Footer/>
+          </main>
         </div>
-    )
-};
+      </div>
+      <Footer />
+    </div>
+  )
+}
 
 export default App
